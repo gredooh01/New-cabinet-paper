3fdfa697d9d5bf3d4a5ca73a7917e76edd0ba09c
Laravel Daily <info@laraveldaily.com>
2023-03-29 23:18:49 +0000

    QuickAdminPanel automatic commit

---
+++ app/Http/Requests/UpdateCabinetPaperRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\CabinetPaper;
+use Gate;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Response;
+
+class UpdateCabinetPaperRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return Gate::allows('cabinet_paper_edit');
+    }
+
+    public function rules()
+    {
+        return [
+            'cabinet_type' => [
+                'required',
+            ],
+            'cabinet_num' => [
+                'required',
+                'integer',
+                'min:-2147483648',
+                'max:2147483647',
+            ],
+            'issue_date' => [
+                'required',
+                'date_format:' . config('panel.date_format'),
+            ],
+            'dispatch_date' => [
+                'required',
+                'date_format:' . config('panel.date_format'),
+            ],
+            'archive_date' => [
+                'required',
+                'date_format:' . config('panel.date_format'),
+            ],
+            'cabinet_title' => [
+                'string',
+                'required',
+            ],
+            'cabinet_ministry' => [
+                'string',
+                'required',
+            ],
+        ];
+    }
+}
